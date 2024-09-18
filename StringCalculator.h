2be4e9c23f598440a3ba58647a8c916f7d871a13
rukminiraob <braorukmini@gmail.com>
2024-09-18 19:59:05 +0530

    delimiter and empty string

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,10 +1,21 @@
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h> 
 
 int add(const char*  input)
 {
-  if (strlen(input) == 0)
-  {
-  return 0;
+  char*  string = strdup(input);
+  const char delimiter[2] = ",";
+  char* token;
+  
+  // get the first token
+   token = strtok(string, delimiter);
+  // walk through other tokens
+  while( token != NULL ) {
+    printf( " %s\n", token );
+    int num1 = atoi(token);
+    token = strtok(NULL, delimiter);
   }
-  return 255;
+  
+  return 0;
 }
